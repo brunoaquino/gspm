521bd2b8e0e1593148f3428a5d746807d3345611
bruno-smv2@hotmail.com <Bruno Carvalho>
2016-01-05 11:29:31 -0200

    Criando userType

---
+++ src/main/java/br/com/jsbse/gspm/mvc/tipos/usertype/TipoUsuarioUserType.java
@@ -0,0 +1,8 @@
+package br.com.jsbse.gspm.mvc.tipos.usertype;
+
+import br.com.jsbse.gspm.mvc.tipos.TipoUsuario;
+import br.com.jsbse.integracao.hibernate.tipo.EnumUserType;
+
+public class TipoUsuarioUserType extends EnumUserType<TipoUsuario> {
+
+}
